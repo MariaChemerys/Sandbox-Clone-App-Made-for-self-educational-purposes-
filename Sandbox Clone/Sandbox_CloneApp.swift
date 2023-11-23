597d5b129ba0e2187a027178afe8a972fa4896d2
Maria Chemerys <129984077+MariaChemerys@users.noreply.github.com>
2023-11-23 12:46:56 +0100

    First commit

---
+++ Sandbox Clone/Sandbox_CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sandbox_CloneApp.swift
+//  Sandbox Clone
+//
+//  Created by Mariia Chemerys on 16.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct Sandbox_CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MainView()
+        }
+    }
+}
